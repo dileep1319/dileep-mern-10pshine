e053d5f06b7a6913e8ee586d92e5356228bcf1db
Dileep <dileepkumar18311@gmail.com>
2025-10-26 00:15:17 +0500

    add pino-logger

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
-const app = require('./app');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
+const app = require("./app");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const logger = require("./utils/logger"); // <-- added
 
 dotenv.config();
 
@@ -9,5 +10,5 @@ connectDB();
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  logger.info(`🚀 Server running on port ${PORT} in ${process.env.NODE_ENV} mode`);
 });
